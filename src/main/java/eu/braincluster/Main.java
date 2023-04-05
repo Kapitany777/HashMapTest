23bc598513dff6445ee6c8b9601094160ec07c6d
Viktor Török <torokv@vnet.hu>
2023-04-05 11:22:06 +0200

    Adding example2 and example3

--- src/main/java/eu/braincluster/Main.java
+++ src/main/java/eu/braincluster/Main.java
@@ -19,12 +19,47 @@ public class Main
         }
     }
 
+    public static void example2()
+    {
+        System.out.println("HashMap example 2");
+
+        var map = new HashMap<String, Integer>();
+
+        map.put("Pistaba", 1970);
+        map.put("Feriba", 1975);
+
+        map.forEach((key, value) -> System.out.println(key + " = " + value));
+    }
+
+    public static void example3()
+    {
+        System.out.println("HashMap example 3");
+
+        var squares = new HashMap<Integer, Integer>();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            squares.put(i, i * i);
+        }
+
+        squares
+                .entrySet()
+                .stream()
+                .forEach(e -> System.out.println(e.getKey() + "^2 = " + e.getValue()));
+    }
+
     public static void main(String[] args)
     {
         System.out.println("HashMap test program");
         System.out.println();
 
         example1();
+        System.out.println();
+
+        example2();
+        System.out.println();
+
+        example3();
         System.out.println();
     }
 }
